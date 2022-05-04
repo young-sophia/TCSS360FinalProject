06e4a28721b073190ae99e3ceb44cb8d61cfe918
Arashpreet S. Pandher <pandherarashpreet@gmail.com>
2022-05-03 17:51:11 -0700

    added src files

---
+++ src/Maze.java
@@ -0,0 +1,14 @@
+
+public class Maze {
+    private int[][] myMaze;
+    private int myRows;
+    private int myColumns;
+
+    public Maze(final int theRows, final int theColumns){
+        myRows = theRows;
+        myColumns = theColumns;
+        myMaze = new int[theRows][theColumns];
+
+    }
+
+}
---
+++ src/Room.java
@@ -0,0 +1,23 @@
+/**
+ * Function similar to edge
+ */
+public class Room {
+    private boolean theVisted;
+    enum Direction{
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
+
+    private Direction direction;
+
+
+    public Room(){
+        if()
+    }
+
+    public boolean Visited(){
+
+    }
+}
